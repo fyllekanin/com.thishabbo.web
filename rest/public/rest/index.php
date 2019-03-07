f18228c3d3e728ef2fbe2774b4b65402ba6fb370
eric <eric.christensen94@gmail.com>
2019-03-07 17:58:08 +0100

    And another one

--- rest/public/rest/index.php
+++ rest/public/rest/index.php
@@ -2,7 +2,7 @@
 
 /** If cloudflare connection IP is set, then set it on REMOTE_ADDR */
 if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
-    $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+    $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
 }
 
 /**
