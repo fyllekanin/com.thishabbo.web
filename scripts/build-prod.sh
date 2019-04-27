4970ded39a5067f2921879404d34c389035fac59
Eric <eric.christensen94@gmail.com>
2019-04-27 13:04:45 +0200

    Remove job-runner call

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -23,5 +23,4 @@ cp -r rest target
 # Move front-end into public folder
 mv ./front-end/dist/thvx/* ./target/public
 cp ./front-end/.htaccess ./target/public/.htaccess
-cp ./scripts/job-runner.sh ./target/job-runner.sh
 
