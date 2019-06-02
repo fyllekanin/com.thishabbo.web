616b476414ec1b88058a5616f56ffc0a17e3c601
Andy <mirageboss@gmail.com>
2019-06-03 00:48:27 +0800

    Remove Useless Import

--- front-end/src/app/pages/staff/sub-pages/radio/connection/connection.component.ts
+++ front-end/src/app/pages/staff/sub-pages/radio/connection/connection.component.ts
@@ -7,7 +7,6 @@ import { Breadcrumb } from 'core/services/breadcrum/breadcrum.model';
 import { STAFFCP_BREADCRUM_ITEM, STAFFCP_RADIO_BREADCRUM_ITEM } from '../../../staff.constants';
 import { AuthService } from 'core/services/auth/auth.service';
 import { StringHelper } from 'shared/helpers/string.helper';
-import { stringify } from '@angular/compiler/src/util';
 
 @Component({
     selector: 'app-staff-radio-connection',
