a9fa65535a54815f8af59a6b31349e164e755db0
Andy <mirageboss@gmail.com>
2019-06-09 18:34:29 +0800

    Sonar Fixes for more files

--- front-end/src/app/pages/custom/leader-board/leader-board.component.ts
+++ front-end/src/app/pages/custom/leader-board/leader-board.component.ts
@@ -1,15 +1,16 @@
-import { Component, ElementRef, OnDestroy } from '@angular/core';
-import { Page } from 'shared/page/page.model';
-import { BreadcrumbService } from 'core/services/breadcrum/breadcrumb.service';
-import { Breadcrumb } from 'core/services/breadcrum/breadcrum.model';
+import {Component, ElementRef, OnDestroy} from '@angular/core';
+import {Page} from 'shared/page/page.model';
+import {BreadcrumbService} from 'core/services/breadcrum/breadcrumb.service';
+import {Breadcrumb} from 'core/services/breadcrum/breadcrum.model';
 
 @Component({
     selector: 'app-custom-leader-board',
-    templateUrl: 'leader-board.component.html'
+    templateUrl: 'leader-board.component.html',
+    styleUrls: ['leader-board.component.css']
 })
 export class LeaderBoardComponent extends Page implements OnDestroy {
 
-    constructor (
+    constructor(
         elementRef: ElementRef,
         breadcrumbService: BreadcrumbService
     ) {
@@ -17,7 +18,7 @@ export class LeaderBoardComponent extends Page implements OnDestroy {
         breadcrumbService.breadcrumb = new Breadcrumb({current: 'Leader board'});
     }
 
-    ngOnDestroy () {
+    ngOnDestroy() {
         super.destroy();
     }
 }
