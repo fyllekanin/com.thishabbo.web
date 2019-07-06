d4b2167950f3f2d24e07482b5ef8362849015f26
Eric <eric.christensen94@gmail.com>
2019-07-06 11:25:52 +0200

    Fix lint

--- front-end/src/app/shared/helpers/number.helper.ts
+++ front-end/src/app/shared/helpers/number.helper.ts
@@ -3,7 +3,7 @@ export class NumberHelper {
     static random (min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
-    
+
     static numberWithCommas (num: number): string {
         return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     }
