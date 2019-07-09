c7b15c0054816a02c69ec9d4adaf497134af0302
Eric <eric.christensen94@gmail.com>
2019-07-09 13:59:24 +0200

    Use DB facade

--- rest/database/migrations/2019_07_08_235503_add_video_b_b_code.php
+++ rest/database/migrations/2019_07_08_235503_add_video_b_b_code.php
@@ -1,38 +1,34 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use App\EloquentModels\BBCode;
+use Illuminate\Support\Facades\DB;
 
-class AddVideoBBCode extends Migration
-{
+class AddVideoBBCode extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        $bbcode = new BBCode ([
+    public function up() {
+        DB::table('bbcodes')->insert([
             'name' => 'Video',
             'example' => '[video]dQw4wp9WgXcQ[/video]',
             'pattern' => '/\[video\](.*?)\[\/video\]/si',
             'replace' => '<iframe width="560" height="315" frameborder="0" src="https://www.youtube.com/embed/$1?wmode=opaque"'
                 . 'data-youtube-id="$1" allowfullscreen=""></iframe>',
             'content' => '$1',
-            'isSystem' => '1'
+            'isSystem' => '1',
+            'createdAt' => time(),
+            'updatedAt' => time()
         ]);
-
-        $bbcode->save();
     }
+
     /**
      * Reverse the migrations.
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
 
     }
 }
