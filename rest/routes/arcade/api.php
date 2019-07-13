d332916246814f4be8538d33f2f40ba458d905ec
Eric <eric.christensen94@gmail.com>
2019-07-13 21:53:20 +0200

    Fix habbo imager and roulette minimum

--- rest/routes/arcade/api.php
+++ rest/routes/arcade/api.php
@@ -1,6 +1,6 @@
 <?php
 
-/*use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
 
 Route::get('fast-typer/highscore', 'Arcade\FastTyperController@getFastTyperHighscoreTable');
 Route::get('fast-typer', 'Arcade\FastTyperController@getFastTyperParagraph');
@@ -8,4 +8,4 @@ Route::post('fast-typer', 'Arcade\FastTyperController@createFastTyperScore');
 
 Route::get('snake/highscore', 'Arcade\SnakeController@getSnakeHighscoreTable');
 Route::get('snake', 'Arcade\SnakeController@getSnakeGame');
-Route::post('snake', 'Arcade\SnakeController@createSnakeScore');*/
+Route::post('snake', 'Arcade\SnakeController@createSnakeScore');
