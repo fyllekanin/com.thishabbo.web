edca12105b24548f70382bebbd06f628b1d64133
Eric <eric.christensen94@gmail.com>
2019-07-13 11:51:08 +0200

    Fix whitespace...

--- front-end/src/app/pages/staff/sub-pages/management/current-listeners/current-listeners.component.ts
+++ front-end/src/app/pages/staff/sub-pages/management/current-listeners/current-listeners.component.ts
@@ -53,7 +53,6 @@ export class CurrentListenersComponent extends Page implements OnDestroy {
         const hours = Math.floor(time / 3600);
         const timeForMinutes = time - (hours * 3600);
         const minutes = Math.floor(timeForMinutes / 60);
-        
         return `${hours} hours and ${minutes} min(s)`;
     }
 
