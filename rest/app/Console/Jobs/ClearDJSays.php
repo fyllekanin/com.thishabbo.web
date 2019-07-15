a3c5b8099feb3c8a26987b83e0f92aef208b5b29
Andy <mirageboss@gmail.com>
2019-07-15 08:56:47 +0800

    Update ClearDJSays.php

--- rest/app/Console/Jobs/ClearDJSays.php
+++ rest/app/Console/Jobs/ClearDJSays.php
@@ -11,7 +11,7 @@ class ClearDJSays {
     public function init() {
         $settingKeys = ConfigHelper::getKeyConfig();
         $radio = new RadioSettings(SettingsHelper::getSettingValue($settingKeys->radio));
-        $radio->djSays = '';
+        $radio->djSays = 'DJ says has not been set!';
         SettingsHelper::createOrUpdateSetting($settingKeys->radio, json_encode($radio));
     }
 }
