52a27e18eb6f0b343881d174ccfc3c8156a32889
Eric <eric.christensen94@gmail.com>
2019-07-29 20:50:08 +0200

    add logger

---
+++ rest/job/discord/logger.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const TOKEN = 'MjkyMDQxMjA1MTcxMDkzNTA0.XT8fjA.aDK7u-7xvoTYdW0O82-byoMX04I';
+let CHANNEL = null;
+
+const events = [
+    'message',
+    'messageDelete'
+];
+
+client.on('ready', () => {
+    console.log(`Discord bot is ready`);
+    CHANNEL = client.channels.get('587240431683764224');
+});
+
+
+events.forEach(event => {
+    client.on(event, data => {
+        if (!CHANNEL || data.channel.id === CHANNEL.id) {
+            return;
+        }
+        let message = '**Log**\n';
+        message += `User: ${data.author.username}#${data.author.discriminator} - ${event === 'message' ? 'sent' : 'deleted'} in channel: **${data.channel.name}**\n`;
+        message += '```';
+        message += data.content;
+        message += '```';
+        CHANNEL.send(message);
+    });
+});
+
+client.login(TOKEN);
