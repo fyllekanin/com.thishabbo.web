aaa68da96429a7b8fb01a36b410b7a97c1bcb3ef
Eric <eric.christensen94@gmail.com>
2019-08-13 19:47:53 +0200

    Update threads title length

---
+++ rest/database/migrations/2019_08_13_174658_change_title_length.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeTitleLength extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('threads', function (Blueprint $table) {
+            $table->text('title')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('threads', function (Blueprint $table) {
+            $table->string('title')->change();
+        });
+    }
+}
