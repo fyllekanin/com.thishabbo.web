7e7579f751c356ecdb91d15decaa7ae6066dbf19
Eric <eric.christensen94@gmail.com>
2019-09-14 16:53:48 +0200

    Update constants

--- rest/constants.php
+++ rest/constants.php
@@ -8,12 +8,12 @@ define('CONST_APP_LOCALE', 'en');
 
 define('CONST_LOG_CHANNEL', 'stack');
 
-define('CONST_DB_CONNECTION', 'sqlite');
+define('CONST_DB_CONNECTION', 'mysql');
 define('CONST_DB_HOST', '127.0.0.1');
 define('CONST_DB_PORT', '3306');
 define('CONST_DB_DATABASE', 'thsite');
 define('CONST_DB_USERNAME', 'root');
-define('CONST_DB_PASSWORD', '');
+define('CONST_DB_PASSWORD', 'jenkins');
 
 define('CONST_CACHE_DRIVER', 'array');
 define('CONST_QUEUE_CONNECTION', 'database');
@@ -22,3 +22,4 @@ define('CONST_QUEUE_TABLE', 'jobs');
 define('CONST_PAYPAL_CLIENT_ID', 'AY93kR0Gr1VxHYUKW47FNfu1dwMjwgOQ5oKjLlM8Iiwkku8UvgOqJ9sj0C7jrzjiIL1vRdbzqmoMHxKW');
 define('CONST_PAYPAL_SECRET', 'EAcCmr13_6usxNHsWD2Ejf1VVohheUbr0OdgQc0x1u4NGVUb4H2A1slM2ZrRoIvp7lpt8NDqU1txAQ6c');
 define('CONST_PAYPAL_MODE', 'sandbox');
+
